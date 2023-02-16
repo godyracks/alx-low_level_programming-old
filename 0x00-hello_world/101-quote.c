f5b9907246baf342a92d9eebad09539e01842f31
GodyRacks <godfreymatagaro@gmail.com>
2023-02-17 00:56:31 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -A c program that prints a line to the standard error
- * Return 1 (Success)
+ *main - Entry point
+ *Return: 1
  */
 int main(void)
 {

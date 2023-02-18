e22fcc0fc6ff9d66629e0eb038100aa8f6e57320
GodyRacks <godfreymatagaro@gmail.com>
2023-02-18 11:41:22 +0300

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,20 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
-	
+
 	c = 'a';
 	while
-		(c <= 'z') {
-			putchar(c);                                                                                                                                            			    c++;
+		(c <= 'z')
+	       	{
+			putchar(c);
+			c++;
 		}
 	putchar('\n');
 	return (0);
 }
+
+
